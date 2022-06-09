9168cab271eb637091a48ba31be7ecd7d56d6f42
Tom Targosz <tom.targosz@yahoo.com>
2022-06-09 09:10:57 -0500

    update example

--- examples/App.tsx
+++ examples/App.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {ContentScroller} from 'react-native-content-scroller';
 
 const App = () => {
-  const isDarkMode = false;
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <View style={{backgroundColor: 'white', flex: 1, justifyContent: 'center'}}>
       <ContentScroller
         messages={[
-          <View style={{height: 25, backgroundColor: 'blue'}}>
-            <Text>Hello!</Text>
+          <View
+            style={{
+              height: 100,
+              backgroundColor: 'blue',
+              justifyContent: 'center',
+            }}>
+            <Text>One</Text>
           </View>,
-          <View style={{height: 25, backgroundColor: 'red'}}>
-            <Text>There!</Text>
+          <View
+            style={{
+              height: 100,
+              backgroundColor: 'green',
+              justifyContent: 'center',
+            }}>
+            <Text>Two</Text>
+          </View>,
+          <View
+            style={{
+              height: 100,
+              backgroundColor: 'red',
+              justifyContent: 'center',
+            }}>
+            <Text>Three</Text>
           </View>,
         ]}
-        numberOfMessagesToDisplay={2}
-        rotationInterval={2500}
+        numberOfMessagesToDisplay={3}
+        rotationInterval={1500}
       />
     </View>
   );
